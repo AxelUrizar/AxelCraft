5086dd25b21741fa8fe63b749bb91620fd6f8e27
AxelUrizar <axelurizarmartin@gmail.com>
2023-02-09 19:41:51 +0100

    Logo change

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    server: {
-        port: process.env.PORT || '8000'
-    }
 })
